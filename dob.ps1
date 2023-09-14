f2b6ca21c7d0d3ae48a605090d6343b0f417a833
Iamneo <techadmin@iamneo.ai>
2023-09-14 16:41:32 +0000

    Backup Commit

--- dob.ps1
+++ dob.ps1
@@ -1,9 +1,9 @@
-$day = Read-Host "enter the day :"
-$month = Read-Host "enter the month:"
-$year = Read-Host "enter the year:"
+$day = Read-Host "enter the day (DD) :"
+$month = Read-Host "enter the month (MM):"
+$year = Read-Host "enter the year (YYYY):"
 
-if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year - match '^\d{4}$' -and [int]$day -le 31 -and [int]$month -le 12) {
-    $dateOfBirth = Get-Date -Year $year -Month $month -Day $day
+if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year - match '^\d{4}$') {
+    $dateString = $day.PadLeft(2, '0') + '-' + $month
 
     $formattedDate = $dateOfBirth.ToString("dd-MM-yyyy")
 

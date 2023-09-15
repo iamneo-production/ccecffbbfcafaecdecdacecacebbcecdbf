f5fe266d6bec75a3b4eb92ac7cd2025c2a698295
Iamneo <techadmin@iamneo.ai>
2023-09-15 01:12:10 +0000

    Backup Commit

--- dob.ps1
+++ dob.ps1
@@ -1,13 +1,16 @@
 $day = Read-Host "enter the day (DD) :"
 $month = Read-Host "enter the month (MM):"
 $year = Read-Host "enter the year (YYYY):"
+$day = [int]$day
+$month = [int]$month
+$year = [int]$year
 
 if ($day -match '^\d{1,2}$' -and $month -match '^\d{1,2}$' -and $year - match '^\d{4}$') {
     $dateString = $day.PadLeft(2, '0') + '-' + $month
 
     $formattedDate = $dateOfBirth.ToString("dd-MM-yyyy")
 
-    Write-Host "Date of Birth: $formattedDate"
+    Write-Host "Date of Birth: $dateString"
 }else{
-    Write-Host "Invalid input. Please enter valid day (1-31), month (1-12), and year (4 digits)."
+    Write-Host "Invalid input. Please enter valid day (1-31), month (1-12), and year (19000-2099)."
 }
